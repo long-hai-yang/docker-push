7cbb814ed1823aab89c42d6c9a05576ed6befca6
龙海洋 <18602860304@qq.com>
2017-09-06 17:50:16 +0800

    +第一次提交代码

---
+++ README.md
@@ -0,0 +1,19 @@
+# docker-push
+安装使用:
+<pre><code>npm install -g docker-push</code></pre>
+参数：
+<pre><code>
+  Usage: docker-push [options]
+
+  //docker-push
+
+  Options:
+
+    -V, --version      //output the version number输出版本号
+    -u, --username <sv>     //your username你登录dokcer的账号
+    -p, --password <password>    //your password你登录dokcer的密码
+    -ad, --serveraddress <serveraddress>  //your serveraddress你服务的地址
+    -n, --name <name>  //your name 你的docker包名
+    -url, --url <url>  //your url你服务器的地址
+    -h, --help         //output usage information帮助文档
+</code></pre>
---
+++ index.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+var localFile=require('./package.json');
+var request = require('request');
+var program = require('commander')
+program
+    .version(localFile.version)
+    .allowUnknownOption()
+    .description('docker-push')
+    .option('-u, --username <username>', 'your username')
+    .option('-p, --password <password>', 'your password')
+    .option('-ad, --serveraddress <serveraddress>', 'your serveraddress')
+    .option('-n, --name <name>', 'your name')
+    .option('-url, --url <url>', 'your url')
+program.parse(process.argv)
+var _auth=new Buffer({"username":program.username,"password":program.password,"serverAddress":program.serveraddress});
+_auth=_auth.toString('base64');
+// console.log(program.name);
+push(_auth);
+// request({
+//     url: program.url+'/auth',
+//     method: "POST",
+//     json: true,
+//     headers: {
+//         "content-type": "application/json",
+//     },
+//     body: {
+//         "username": program.username,
+//         "password": program.password,
+//         "serveraddress": program.serveraddress
+//     }
+// }, function(error, response, body) {
+//     // console.log(response.statusCode);
+//     if (!error && response.statusCode == 200) {
+//         // console.log(body);
+//         push(body.IdentityToken);
+//     }else{
+//         console.log('find not identityToken');
+//         process.exit(1);
+//     }
+// }); 
+function push(){
+    let star=program.name.indexOf(':');
+    let len=program.name.indexOf(':',star+1);
+    let name=program.name.substring(0,len);
+    var tag=program.name.substring(len+1,program.name.length);
+    console.log(name);
+    console.log(tag);
+    request({
+        url: program.url+'/images/'+name+'/push?tag='+tag,
+        method: "POST",
+        json: true,
+        headers: {
+            "X-Registry-Auth": _auth,
+        }
+    }, function(error, response, body) {
+        console.log(body);
+        if (!error && response.statusCode == 200) {
+            console.log('push success');
+            process.exit(0);
+        }else{
+            console.log('push error');
+            process.exit(1);
+        }
+    }); 
+}
+
